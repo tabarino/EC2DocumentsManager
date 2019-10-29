1908876007711206004d5f721d3697e6dce1335e
Ivan <tabarino@outlook.com>
2019-10-29 12:49:41 +0000

    Add Docker Configuration

---
+++ docker/start.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+env=${APP_ENV:-production}
+role=${CONTAINER_ROLE:-app}
+
+echo "The environment is $env"
+
+if [ "$env" != "local" ]; then
+    echo "Removing Xdebug"
+    rm -rf /usr/local/etc/php/conf.d/{docker-php-ext-xdebug.ini,xdebug.ini}
+fi
+
+echo "The container role is $role"
+
+if [ "$role" = "app" ]; then
+    exec apache2-foreground
+else
+    echo "Could not match the container role \"role\""
+    exit 1
+fi
